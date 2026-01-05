c97848bacd6c4a0fd48203c2afd4059f12f89334
Mahmoud <mahmoudabbas393@gmail.com>
2026-01-05 12:13:36 +0200

    Add response interceptor to handle 401 Unauthorized errors
    - Implemented a response interceptor in axiosInstance to catch 401 errors.
    - On receiving a 401 status, the interceptor clears the authentication token from cookies and redirects the user to the signin page.

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Cookies } from "react-cookie";
 
 axios.defaults.baseURL = "https://admin.mycn.online/api";
 axios.defaults.headers.common["Content-Type"] = "application/json";
@@ -13,6 +14,22 @@ axiosInstance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+    console.log(status);
+
+    if (status === 401) {
+      document.cookie = "token=; Max-Age=0; path=/;";
+      window.location.href = "/signin";
+    }
+
     return Promise.reject(error);
   }
 );
